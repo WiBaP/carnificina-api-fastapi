cbb95de04a353c352e6009427890a48a5db04ba3
Willian Batista <willianbp96@gmail.com>
2025-08-25 00:09:20 -0300

    Preparando deploy para Vercel, ajustes de db e main

--- db/db.py
+++ db/db.py
@@ -1,24 +1,16 @@
 from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
 import os
 
-USER = os.environ.get("USER")
-PASSWORD = os.environ.get("PASSWORD")
-HOST = os.environ.get("HOST")
-PORT = os.environ.get("PORT")
-DBNAME = os.environ.get("DBNAME")
+# Carrega .env da raiz do projeto
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
+
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = int(os.getenv("PORT"))  # ⬅️ converter para int aqui
+DBNAME = os.getenv("DBNAME")
 
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
-
-try:
-    with engine.connect() as connection:
-        result = connection.execute(text("SELECT 1;"))
-        for row in result:
-            print("Test query result:", row)
-
-        print("Connection successful!")
-        print("USER:", USER)
-        print("HOST:", HOST)
-except Exception as e:
-    print(f"Failed to connect: {e}")
