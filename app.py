9c4f99aec42fae487dabb1d2b04ef2c0d37d1e91
Willian Batista <willianbp96@gmail.com>
2025-08-27 23:49:33 -0300

    Preparando deploy para Vercel, ajustes de db e main

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],       # De onde aceitar requisições ("*" = qualquer site)
+    allow_origins=["https://carnificina-api-fastapi.vercel.app"],       # De onde aceitar requisições ("*" = qualquer site)
     allow_credentials=True,    # Permite enviar cookies/autenticação
     allow_methods=["*"],       # Permite todos os métodos HTTP (GET, POST, etc.)
     allow_headers=["*"],       # Permite todos os headers
